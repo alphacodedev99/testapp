d3c380e9b39f17608d0fffd52770e97c4ce8e6a2
alphacodedev99 <kiza.milenkovic@gmail.com>
2024-06-17 17:16:42 +0200

    ovo je izmena na mom projektu

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  
+  function handleClick(){
+    alert('CAO FANTAA')
+  }
+ 
+
+  return (
+   <div>
+        <button onClick={handleClick}>Klikni Me i Pokreni Zabavu FANTA</button>
+   </div>
+  )
+}
+
+export default App
